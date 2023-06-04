80459a9797de7ddfd3a3ae2e58de0c96e08696e6
Maximo <91247@sistemas.frc.utn.edu.ar>
2023-06-03 23:30:17 -0300

    Banners

--- clothing_page/src/containers/Home.js
+++ clothing_page/src/containers/Home.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import "../assets/css/Home.css"
-
+import banner from "../assets/statics/banner-newArrival.png"
+import banner_sale from "../assets/statics/banner-oferta.png"
+import next from "../assets/statics/next_black.png"
+import next_w from "../assets/statics/next_white.png"
+import { useState } from 'react'
 
 export default function Home() {
+  const [mostrarBanner, setMostrarBanner] = useState(true);
+
+  const nextBanner = () => {
+    setMostrarBanner(false);
+  };
+
+  const backBanner = () => {
+    setMostrarBanner(true);
+  };
   return (
     <div>
-        
-    </div>
+      {mostrarBanner ? (
+        <div>
+          <img id="banner" src={banner} alt="" />
+          <button id='next-banner' onClick={nextBanner}><img src={next} alt="" /></button>
+          <button id='back-banner' onClick={backBanner}>ATRAS</button>
+        </div>
+      ) : (
+        <div>
+          <img id="banner-sale" src={banner_sale} alt="" />
+          <button id='next-banner' onClick={nextBanner}><img src={next_w} alt="" /></button>
+          <button id='back-banner' onClick={backBanner}>ATRAS</button>
+        </div>
+      )}
+  </div>
   )
 }
