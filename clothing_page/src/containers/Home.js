54a132c3de95934ccd1e2809e30b180ada9f5f43
Maximo <91247@sistemas.frc.utn.edu.ar>
2023-06-04 20:10:09 -0300

    Banner terminado

--- clothing_page/src/containers/Home.js
+++ clothing_page/src/containers/Home.js
@@ -2,35 +2,64 @@ import React from 'react'
 import "../assets/css/Home.css"
 import banner from "../assets/statics/banner-newArrival.png"
 import banner_sale from "../assets/statics/banner-oferta.png"
-import next from "../assets/statics/next_black.png"
-import next_w from "../assets/statics/next_white.png"
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Home() {
   const [mostrarBanner, setMostrarBanner] = useState(true);
 
+
   const nextBanner = () => {
-    setMostrarBanner(false);
+    setMostrarBanner((prevMostrarBanner) => !prevMostrarBanner);
   };
+  
 
   const backBanner = () => {
-    setMostrarBanner(true);
+    setMostrarBanner((prevMostrarBanner) => !prevMostrarBanner);
   };
+  
+
+  // Cambiar el banner cada 5 segundos
+
+  useEffect(() => {
+    const cambiarBannerTimeout = setTimeout(() => {
+      setMostrarBanner((prevState) => !prevState);
+    }, 8000);
+
+    return () => clearTimeout(cambiarBannerTimeout);
+  }, [mostrarBanner]);
+
   return (
     <div>
-      {mostrarBanner ? (
-        <div>
-          <img id="banner" src={banner} alt="" />
-          <button id='next-banner' onClick={nextBanner}><img src={next} alt="" /></button>
-          <button id='back-banner' onClick={backBanner}>ATRAS</button>
-        </div>
-      ) : (
-        <div>
-          <img id="banner-sale" src={banner_sale} alt="" />
-          <button id='next-banner' onClick={nextBanner}><img src={next_w} alt="" /></button>
-          <button id='back-banner' onClick={backBanner}>ATRAS</button>
-        </div>
-      )}
+      <div id='cont-banner'>
+        {mostrarBanner ? (
+          <div>
+            <img id="banner" src={banner} alt="" />
+            <button id='next-banner' onClick={nextBanner}><FontAwesomeIcon icon={faChevronRight}/></button>
+            <button id='back-banner' onClick={backBanner}><FontAwesomeIcon icon={faChevronLeft}/></button>
+          </div>
+        ) : (
+          <div>
+            <img id="banner-sale" src={banner_sale} alt="" />
+            <button id='next-banner' onClick={nextBanner}><FontAwesomeIcon icon={faChevronRight} style={{color: "#ffffff"}} /></button>
+            <button id='back-banner' onClick={backBanner}><FontAwesomeIcon icon={faChevronLeft} style={{color: "#ffffff"}} /></button>
+          </div>
+        )}
+    </div>
+    <div id='products-featured'>
+      <div className="cont-product">
+        <div className='box-product'></div>
+        <div className='box-product'></div>
+        <div className='box-product'></div>
+      </div>
+      <div className="cont-product">
+        <div className='box-product'></div>
+        <div className='box-product'></div>
+        <div className='box-product'></div>
+      </div>
+    </div>
   </div>
   )
 }
